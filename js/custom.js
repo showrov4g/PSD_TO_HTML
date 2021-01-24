8c5f4d6848c5323a057898093a4210763082f4ad
Showrov Ghosh <71726240+showrov644156@users.noreply.github.com>
2021-01-25 00:55:56 +0600

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,7 @@
 (function($){
     $(document).ready(function(){
-    
+         $(".header-area .menu ul ul li").hover(function(){
+             $(".header-area .menu ul ul li ul").show();
+         });
     });
 })(jQuery)
